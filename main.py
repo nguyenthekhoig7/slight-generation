5ce0cc1a8a476b3137ccf97348610ca0f82bbc95
duongttr <duongdayne1909@gmail.com>
2023-06-08 14:31:02 +0700

    protect api with token

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-import sys, os, glob
+import sys, os
 from PIL import Image
-import collections.abc
 from typing import Optional
 from fastapi import FastAPI
 from pptx.util import Inches
 from pptx import Presentation
 from api_key import *
-# from bing_image_downloader import downloader
 from bing_image_urls import bing_image_urls
 
 from src.utils import *
 from src.text_gen import *
 
-import requests
-
 import random
 import io
 import urllib
-from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
 
 
@@ -35,11 +32,11 @@ app = FastAPI()
 
 # middlewares
 app.add_middleware(
-    CORSMiddleware, # https://fastapi.tiangolo.com/tutorial/cors/
-    allow_origins=['*'], # wildcard to allow all, more here - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
-    allow_credentials=True, # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Credentials
-    allow_methods=['*'], # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Methods
-    allow_headers=['*'], # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Headers
+    CORSMiddleware, 
+    allow_origins=['*'], 
+    allow_credentials=True, 
+    allow_methods=['*'], 
+    allow_headers=['*'],
 )
 
 # import nest_asyncio
@@ -58,18 +55,22 @@ app.add_middleware(
 
 # nest_asyncio.apply()
 
-
-
+class Input(BaseModel):
+    topic : str
+    mode : int = 0
+    n_slides : Optional[int] = 10
+    n_words_per_slide : Optional[int] = 70
+    api_token : str = None
 
 @app.post("/generate/")
-async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_words_per_slide: Optional[int] = 70):
+async def generate(inp_params: Input):
     """ """
-    if mode == 0:
-        text_query = create_query_from_text(topic, type_of_text="topic", n_slides=n_slides, n_words_per_slide=n_words_per_slide)
+    if inp_params.mode == 0:
+        text_query = create_query_from_text(inp_params.topic, type_of_text="topic", n_slides=inp_params.n_slides, n_words_per_slide=inp_params.n_words_per_slide)
     else:
-        text_query = create_query_from_text(topic, type_of_text="doc", n_slides=n_slides, n_words_per_slide=n_words_per_slide)
+        text_query = create_query_from_text(inp_params.topic, type_of_text="doc", n_slides=inp_params.n_slides, n_words_per_slide=inp_params.n_words_per_slide)
 
-    response = query_from_API(query=text_query, token=POE_API_KEY)
+    response = query_from_API(query=text_query, token=inp_params.api_token)
     content_json = create_content_json(response)
 
     if content_json is None:
@@ -102,7 +103,7 @@ async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_wo
 
     # get_title_query = create_query_get_title(content_json)
     # title = query_from_API(query=get_title_query, token=POE_API_KEY)
-    title = topic
+    title = inp_params.topic
     slide_title = prs.slides.add_slide(prs.slide_layouts[0])
     title_box = slide_title.shapes.title
     title_box.text = title.capitalize()
@@ -120,10 +121,9 @@ async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_wo
     img_slot = {"left": Inches(6), "top": Inches(1.6), "width": Inches(3.6), "height": Inches(3.6)}
     img_slot_ratio = img_slot["width"] / img_slot["height"]
 
-    returned_content_json = content_json[key]
-    for item in returned_content_json:
+    for item in content_json[key]:
         header, content = process_header(item["header"]), item["content"]
-        image_query = f"{header}_{topic}".replace(" ", "_")
+        image_query = f"{header}_{inp_params.topic}".replace(" ", "_")
         image_pil = None
         if "Introduction" not in image_query:
             try:
@@ -141,7 +141,7 @@ async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_wo
                 # image_path = os.path.join(image_folder_path, os.listdir(image_folder_path)[0])
                 # image = Image.open(image_path)
                 image_urls = bing_image_urls(image_query, limit=10)
-                # random.shuffle(image_urls)
+                random.shuffle(image_urls)
                 for url in image_urls:
                     try:
                         image_content = urllib.request.urlopen(url, timeout=3)
@@ -152,7 +152,7 @@ async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_wo
                     except:
                         print("Cannot download image from url: {}".format(url))
                         continue
-                item["image_url"] = url
+
                 print('Downloaded image {} for slide "{}"'.format(url, header))
             except Exception as e:
                 print(e)
@@ -186,7 +186,7 @@ async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_wo
         text_frame.fit_text(font_file=CHOSEN_FONT, max_size=22)
         text_frame.word_wrap = True
 
-    output_pptx_path = os.path.join("data", topic.replace(" ", "_") + ".pptx")
+    output_pptx_path = os.path.join("data", inp_params.topic.replace(" ", "_") + ".pptx")
     output_pptx_path = change_name_if_duplicated(output_pptx_path)
     prs.save(output_pptx_path)
 
@@ -195,18 +195,10 @@ async def generate(topic: str, mode: int = 0, n_slides: Optional[int] = 10, n_wo
     # convert_pptx_to_svg(pptx_file=output_pptx_path, output_folder=output_folder)
     print(f"Presentation saved to {output_folder}")
 
-    # files_response = []
-    # for file in sorted(glob.glob(os.path.join(output_folder, "*.svg"))):
-    #     files_response.append(FileResponse(file, 
-    #                         filename=file.split("/")[-1], 
-    #                         # media_type="application/vnd.openxmlformats-officedocument.presentationml.presentation")
-    #                         media_type="image/svg+xml")
-    #     )
-    # return files_response
-    file_response = FileResponse(output_pptx_path,
-                                filename=output_pptx_path.split("/")[-1],
-                                media_type="application/vnd.openxmlformats-officedocument.presentationml.presentation")
-    return file_response
+    
+    return FileResponse(output_pptx_path, 
+                        filename=output_pptx_path.split("/")[-1], 
+                        media_type="application/vnd.openxmlformats-officedocument.presentationml.presentation")
 
 # # finally run the app
 # uvicorn.run(app, port=port)
